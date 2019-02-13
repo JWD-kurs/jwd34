a1dd1de64b5f9c15622a781845f953d349f0b98d
Zeljko Vukovic <zeljkov@uns.ac.rs>
2019-02-13 21:02:08 +0100

    lab1-end

---
+++ lab1-end/src/test/java/jwd/wafepa/service/InMemoryActivityServiceTest.java
@@ -0,0 +1,39 @@
+package jwd.wafepa.service;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import jwd.wafepa.model.Activity;
+import jwd.wafepa.service.impl.InMemoryActivityService;
+
+public class InMemoryActivityServiceTest {
+	private ActivityService activityService;
+	
+	@Before
+	public void setUp() {
+		activityService = new InMemoryActivityService();
+		
+		Activity swimming = new Activity("Swimming");
+		Activity running = new Activity("Running");
+		
+		activityService.save(swimming);
+		activityService.save(running);
+	}
+	
+	@Test
+	public void testFindOne() {
+		Activity a = activityService.findOne(2L);
+		
+		Assert.assertEquals("Running", a.getName());
+	}
+	
+	@Test
+	public void testFindAll() {
+		List<Activity> ret = activityService.findAll();
+		
+		Assert.assertEquals(2, ret.size());
+	}
+}
